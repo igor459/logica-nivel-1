dc2977b75260c6a9284e6f122c1fe5bac2951642
igor459 <igorsanta20@gmail.com>
2025-03-18 21:48:08 -0300

    Create ex-09.py

---
+++ ex-09.py
@@ -0,0 +1,6 @@
+#escreva um software que some dois numeros.
+print(' qual o primeiro numero?')
+numero1 = int(input())
+print(' qual o segundo numero?')
+numero2 = int(input())
+print(numero1+numero2)
