319a05701389c03fede5c5f5365745e194da20b3
igor459 <igorsanta20@gmail.com>
2025-04-15 21:46:36 -0300

    Create ex-22.py

---
+++ ex-22.py
@@ -0,0 +1,7 @@
+# Escreva um algoritmo para imprimir os 2 primeiros números inteiros maiores que 100.
+numero = 101
+contador = 0
+while contador < 2:
+   print(numero)
+   numero += 1
+   contador += 1
