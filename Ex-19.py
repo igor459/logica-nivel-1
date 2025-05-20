636dcd5abed409a315f46b6a55e3a920ee6b07cd
igor459 <igorsanta20@gmail.com>
2025-05-20 20:55:28 -0300

    Update Ex-19.py

--- Ex-19.py
+++ Ex-19.py
@@ -1,3 +1,8 @@
+# Ler 3 valores (considere que não serão informados valores iguais) e escrever o maior deles.
+etapas para resolver:
+	1 - peça os 3 valores
+	2 - descobrir e mostrar o maior
+
 valores = []
 for i in range(3):
     valores.append(int(input(f"Digite o {i+1}º valor: ")))
